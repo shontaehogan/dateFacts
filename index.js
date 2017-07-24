6800cc0677947b9f424649a9258ae2d4cba22e52
Shontae Hogan <shontaehogan@gmail.com>
2017-07-24 17:28:36 -0400

    added leap year and daylight savings time

--- index.js
+++ index.js
@@ -24,5 +24,17 @@ let leapYear = moment().isLeapYear()
 
 
 console.log(`It is the ${chalk.blue.bold(now)}`);
-console.log(`It is the ${chalk.rgb(172, 116, 156).bold(dayOfYear) + chalk.rgb(172, 116, 156).bold('th')} day of the year.`)
-console.log(`It is ${chalk.rgb(114, 181, 170).bold(parseInt((current - endOfDay) / 1000))} seconds into the day`)
+console.log(`It is the ${chalk.rgb(172, 116, 156).bold(dayOfYear) + chalk.rgb(172, 116, 156).bold('th')} day of the year.`);
+console.log(`It is ${chalk.rgb(114, 181, 170).bold(parseInt((current - endOfDay) / 1000))} seconds into the day`);
+
+
+if (dst === true) {
+    console.log(`It ${chalk.rgb(142, 168, 99).bold('is')} during Daylight Savings Time`);
+} else {
+    console.log(`It ${chalk.rgb(174, 63, 68).bold('is not')} during Daylight Savings Time`);
+}
+if (leapYear === true) {
+    console.log(`It ${chalk.rgb(142, 168, 99).bold('is')} a leap year.`);
+} else {
+    console.log(`It ${chalk.rgb(174, 63, 68).bold('is not')} a leap year.`);
+}
