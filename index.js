e50d443d88a0edef6eccc0f45b757a8afdd42428
Shontae Hogan <shontaehogan@gmail.com>
2017-07-24 16:54:19 -0400

    initial commit

---
+++ index.js
@@ -0,0 +1,27 @@
+const chalk = require("chalk");
+const moment = require("moment");
+
+console.log(chalk.red("This should be red"));
+
+
+
+// current time
+let now = moment().format("dddd, MMMM Do YYYY, HH:mm:ss a")
+
+// day of the year
+let dayOfYear = moment().format("DDDD")
+
+// seconds into the day
+let secOfDay = moment().format("SSSS")
+let current = moment()
+let endOfDay = moment().startOf("day")
+
+// daylight savings time
+let dst = moment().isDST()
+
+// leap year
+let leapYear = moment().isLeapYear()
+
+
+console.log(`It is the ${chalk.blue.bold(now)}`);
+console.log(`It is the ${chalk.rgb(172, 116, 156).bold(dayOfYear) + chalk.rgb(172, 116, 156).bold('th')} day of the year.`)
